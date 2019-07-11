cfd1daf376a0fbeb5747f5c79c7b4b4003619ef2
lgs <guangshengliang@gmail.com>
2019-07-11 15:25:42 +0800

    init

---
+++ app/src/main/java/com/example/face_android/normal/BFragment.java
@@ -0,0 +1,23 @@
+package com.example.face_android.normal;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.face_android.R;
+
+/**
+ * Created by Jue on 2018/6/2.
+ */
+
+public class BFragment extends android.support.v4.app.Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_b, container,false);
+        return view;
+    }
+}
