388c86225a10eea64df75e069d10aa49055ac448
lgs <guangshengliang@gmail.com>
2021-06-14 23:12:53 +0800

    save

--- app/src/main/java/com/face/utils/CommonUtil.java
+++ app/src/main/java/com/face/utils/CommonUtil.java
@@ -1,16 +1,28 @@
 package com.face.utils;
 
 import android.content.Context;
+import android.util.Log;
 import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.face.enums.account.GenderEnum;
+import com.face.http.BaseObserver;
+import com.face.http.HTTP;
+import com.face.http.model.JsonResponse;
+import com.qiniu.android.common.FixedZone;
+import com.qiniu.android.http.ResponseInfo;
+import com.qiniu.android.storage.Configuration;
+import com.qiniu.android.storage.UpCompletionHandler;
+import com.qiniu.android.storage.UploadManager;
 import face.R;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 import net.sourceforge.pinyin4j.PinyinHelper;
 import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
 import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
 import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
 import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+import org.json.JSONObject;
 
 public class CommonUtil {
     /**
@@ -81,5 +93,41 @@ public class CommonUtil {
         Glide.with(mContext)
                 .load(avatarStr)
                 .into(avatar);
+    }
+
+    public void uploadAvatar() {
+        HTTP.account.getAvatarUploadToken()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new BaseObserver<JsonResponse<String>>() {
+                    @Override
+                    public void onNext(JsonResponse<String> token) {
+
+                    }
+                });
+    }
+
+    public void upload(String token) {
+        Configuration config = new Configuration.Builder()
+//                .zone(FixedZone.zone1)
+                .build();
+        UploadManager uploadManager = new UploadManager(config);
+//        data = <File 对象 或 文件路径 或 字节数组 或 数据流 或 Uri 资源>
+//                String key = <指定七牛服务上的文件名，或 null>;
+        String data="";
+        uploadManager.put(data, null, token,
+                new UpCompletionHandler() {
+                    @Override
+                    public void complete(String key, ResponseInfo info, JSONObject res) {
+                        //res 包含 hash、key 等信息，具体字段取决于上传策略的设置
+                        if (info.isOK()) {
+                            Log.i("qiniu", "Upload Success");
+                        } else {
+                            Log.i("qiniu", "Upload Fail");
+                            //如果失败，这里可以把 info 信息上报自己的服务器，便于后面分析上传错误原因
+                        }
+                        Log.i("qiniu", key + ",\r\n " + info + ",\r\n " + res);
+                    }
+                }, null);
     }
 }
